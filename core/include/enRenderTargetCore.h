0f546d1cea9bc280cc88b2f29a42e381cf9331e9
y1z <whyis2cool@gmail.com>
2020-10-11 17:25:30 -0500

    Added getters for 'enRendertargetCore'.

--- core/include/enRenderTargetCore.h
+++ core/include/enRenderTargetCore.h
@@ -10,20 +10,50 @@ class enRenderTargetCore
 {
 public:
   enRenderTargetCore(const enRenderTargetCore&) = default;
-  enRenderTargetCore(enRenderTargetCore&&) = default;
-  virtual ~enRenderTargetCore() =default;
+  enRenderTargetCore(enRenderTargetCore&&) noexcept = default;
+  virtual ~enRenderTargetCore() noexcept = default;
+
+public:
+  enRenderTargetCore& operator=(const enRenderTargetCore&) = default;
+  enRenderTargetCore& operator=(enRenderTargetCore&&)noexcept = default;
 public:
 
+  /**
+   * @brief : initializes a render target.
+   */
   virtual ErrorCode
   initRenderTarget(const uint32 width,const uint32 height, int format) = 0;
 
 
+  /**
+   * @brief : creates a render-target from a 2D Texture.
+   */
   virtual ErrorCode
-  initFromTexture(const enTexture2DCore& texture) =0;
+  initFromTexture(const enTexture2DCore& texture) = 0;
 
+  /**
+   * @returns The height of the render target
+   */
+  [[nodiscard]] uint32
+  getHeight()const
+  {
+    return m_height;
+  }
+
+  /**
+   * @returns The height of the render target
+   */
+  [[nodiscard]] uint32
+  getWidth()const
+  {
+    return m_width;
+  }
 
 protected:
+  /**@brief The width of the render target*/
   uint32 m_width;
+
+  /**@brief The height of the render target*/
   uint32 m_height;
 };
 
--- core/include/enSwapChainCore.h
+++ core/include/enSwapChainCore.h
@@ -1,15 +1,35 @@
 #pragma once
 #include "core/include/enRenderTargetCore.h"
 
+/**
+ * @class enSwapChainCore : interface for implementation of a swap chain.
+ */
 class enSwapChainCore 
 {
-public:
+public://constructors
+  enSwapChainCore() = default;
+  enSwapChainCore(const enSwapChainCore&) = default;
+  enSwapChainCore(enSwapChainCore&&) noexcept = default;
+  virtual ~enSwapChainCore() noexcept = default;
+public:// operators
 
+  enSwapChainCore& operator= (const enSwapChainCore&) = default;
+  enSwapChainCore& operator= (enSwapChainCore&&) noexcept = default;
+
+public:// functions
+
+  /**
+   * @brief sets the RenderTarget the swap chain will use.
+   */
   virtual ErrorCode
   setRenderTarget(enRenderTargetCore& rt) = 0;
 
 
+  /**
+   * @brief presents all the data draw on to the renderTarget.
+   * @param options : used for when the implementation has additional options also
+   */
   virtual ErrorCode
-  present(int options) = 0;
+  present([[maybe_unused]]int options = 0) = 0;
   
 };
